5a536f855991d7104cd3b8e3c8810c924ac4d9d4
benjieedroso <ben09099@gmail.com>
2024-08-24 20:03:29 +0800

    edit config file vite

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,13 @@ import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/scss/app.css', 'resources/js/app.js'],
+            input: ['resources/scss/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
     resolve: {
         alias: {
             '~bootstrap' : path.resolve(__dirname, 'node_modules/bootstrap'),
-            '~chartjs': path.resolve(__dirname, 'vendor/chart.js'),
-            '~jquery': path.resolve(__dirname, 'vendor/jquery')
         }
     }
 });
